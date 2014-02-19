adc05408e374037da8491de657f4e5cd5308c667
Aleksandar Prokopec <axel22@users.noreply.github.com>
2014-02-19 20:59:07 +0100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,29 @@ object MacroGLBuild extends Build {
 
   /* macro-gl */
 
+  val publishUser = "SONATYPE_USER"
+  
+  val publishPass = "SONATYPE_PASS"
+  
+  val userPass = for {
+    user <- sys.env.get(publishUser)
+    pass <- sys.env.get(publishPass)
+  } yield (user, pass)
+
+  val publishCreds: Seq[Setting[_]] = Seq(userPass match {
+    case Some((user, pass)) =>
+      credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
+    case None =>
+      // prevent publishing
+      publish <<= streams.map(_.log.info("Publishing to Sonatype is disabled since the \"" + publishUser + "\" and/or \"" + publishPass + "\" environment variables are not set."))
+  })
+
   val lwjglVersion = "2.9.0"
 
-  val macroglSettings = Defaults.defaultSettings ++ Seq (
+  val macroglSettings = Defaults.defaultSettings ++ publishCreds ++ Seq (
     name := "macrogl",
     organization := "org.stormenroute",
-    version := "0.0.1",
+    version := "0.0.2-SNAPSHOT",
     scalaVersion := "2.10.2",
     scalacOptions ++= Seq(
       "-deprecation",
@@ -28,7 +45,41 @@ object MacroGLBuild extends Build {
     libraryDependencies ++= Seq(
       "org.lwjgl.lwjgl" % "lwjgl" % lwjglVersion,
       "org.lwjgl.lwjgl" % "lwjgl_util" % lwjglVersion
-    )
+    ),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://storm-enroute.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:storm-enroute/macrogl.git</url>
+        <connection>scm:git:git@github.com:storm-enroute/macrogl.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>
   )
 
   lazy val macrogl = Project(
