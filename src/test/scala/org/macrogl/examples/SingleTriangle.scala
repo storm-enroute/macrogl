722412a035780a2cd2b0739ed132cff4336ddca5
inmost-light <inmost-light@users.noreply.github.com>
2014-02-24 22:59:06 +0300

    Changed MeshBuffer to AttributeBuffer.

--- src/test/scala/org/macrogl/examples/SingleTriangle.scala
+++ src/test/scala/org/macrogl/examples/SingleTriangle.scala
@@ -10,18 +10,19 @@ object SingleTriangle {
     Display.create()
 
     val vertices = Array(
-      -0.5f,  0.5f, 0.0f,  1, 0, 0,  0, 0,
-       0.0f, -0.5f, 0.0f,  0, 1 ,0,  0, 0,
-       0.5f,  0.5f, 0.0f,  0, 0, 1,  0, 0
+      -0.5f,  0.5f, 0.0f,  1, 0, 0,
+       0.0f, -0.5f, 0.0f,  0, 1, 0,
+       0.5f,  0.5f, 0.0f,  0, 0, 1
     )
 
     val fb = BufferUtils.createFloatBuffer(vertices.length)
     fb.put(vertices)
     fb.flip()
 
-    val mb = new MeshBuffer(GL15.GL_STATIC_DRAW, 3)
+    val mb = new AttributeBuffer(GL15.GL_STATIC_DRAW, 3, 6)
     mb.acquire()
     mb.send(0, fb)
+    
     def readResource(path: String) = io.Source.fromURL(getClass.getResource(path)).mkString
 
     val pp = new Program("test")(
@@ -31,15 +32,17 @@ object SingleTriangle {
 
     pp.acquire()
 
+    val attr = Array((0, 3), (3, 3))
+
     while (!Display.isCloseRequested) {
       for {
         _   <- using.program(pp)
-        acc <- using.meshbuffer(mb)
+        acc <- using.attributebuffer(mb)
       } {
         GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f)
         raster.clear(GL11.GL_COLOR_BUFFER_BIT)
 
-        acc.render(GL11.GL_TRIANGLES)
+        acc.render(GL11.GL_TRIANGLES, attr)
       }
 
       Display.update()
