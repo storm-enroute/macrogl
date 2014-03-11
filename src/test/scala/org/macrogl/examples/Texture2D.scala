09f42c6a7b85db2fcbf0adbd89b272899bccbb42
inmost-light <inmost-light@users.noreply.github.com>
2014-03-11 14:25:51 +0300

    Add keyboard controls.

--- src/test/scala/org/macrogl/examples/Texture2D.scala
+++ src/test/scala/org/macrogl/examples/Texture2D.scala
@@ -1,10 +1,17 @@
 package org.macrogl.examples
 
 import org.lwjgl.opengl._
+import org.lwjgl.input.Keyboard
 import org.lwjgl.BufferUtils
 import org.macrogl._
 
 object Texture2D {
+  var texCoord = 1.0f
+  var wrapS = GL11.GL_REPEAT
+
+  var magFilter = GL11.GL_NEAREST
+  var minFilter = GL11.GL_NEAREST
+  
   def main(args: Array[String]) {
     val contextAttributes = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true)
 
@@ -15,16 +22,16 @@ object Texture2D {
     GL30.glBindVertexArray(vao)
 
     val vertices = Array(
-      -0.5f,  0.5f, 0.0f,  0.0f, 0.0f,
-      -0.5f, -0.5f, 0.0f,  0.0f, 1.0f,
-       0.5f,  0.5f, 0.0f,  1.0f, 0.0f
+      -0.5f,  0.5f, 0.0f,  0.0f,     0.0f,
+      -0.5f, -0.5f, 0.0f,  0.0f,     texCoord,
+       0.5f,  0.5f, 0.0f,  texCoord, 0.0f
     )
 
     val fb = BufferUtils.createFloatBuffer(vertices.length)
     fb.put(vertices)
     fb.flip()
 
-    val mb = new AttributeBuffer(GL15.GL_STATIC_DRAW, 3, 5)
+    val mb = new AttributeBuffer(GL15.GL_DYNAMIC_DRAW, 3, 5)
     mb.acquire()
     mb.send(0, fb)
 
@@ -39,8 +46,9 @@ object Texture2D {
       for (_ <- using.texture(GL13.GL_TEXTURE0, tex)) {
         GL11.glTexImage2D(tex.target, 0, GL11.GL_RGB, 64, 64, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, textureBuffer)
 
-        tex.magFilter = GL11.GL_NEAREST
-        tex.minFilter = GL11.GL_NEAREST
+        tex.wrapS = wrapS
+        tex.magFilter = magFilter
+        tex.minFilter = minFilter
       }
     }
 
@@ -58,6 +66,19 @@ object Texture2D {
     val attr = Array((0, 3), (3, 2))
 
     while (!Display.isCloseRequested) {
+      val stateChanged = processInput()
+      if (stateChanged) {
+        vertices(9)  = texCoord
+        vertices(13) = texCoord
+        fb.put(vertices)
+        fb.flip()
+        mb.send(0, fb)
+
+        texture.wrapS = wrapS
+        texture.minFilter = minFilter
+        texture.magFilter = magFilter
+      }
+
       for {
         _   <- using.program(pp)
         _   <- using.texture(GL13.GL_TEXTURE0, texture)
@@ -78,5 +99,35 @@ object Texture2D {
     texture.release()
     mb.release()
     Display.destroy()
+  }
+  def processInput(): Boolean = {
+    var stateChanged = false
+
+    while (Keyboard.next()) {
+      if (!Keyboard.getEventKeyState()) {
+        val ctrlPressed = Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL)
+
+        stateChanged ||= {
+          Keyboard.getEventKey() match {
+            case Keyboard.KEY_1 if ctrlPressed => wrapS = GL12.GL_CLAMP_TO_EDGE;   true
+            case Keyboard.KEY_2 if ctrlPressed => wrapS = GL13.GL_CLAMP_TO_BORDER; true
+            case Keyboard.KEY_3 if ctrlPressed => wrapS = GL14.GL_MIRRORED_REPEAT; true
+            case Keyboard.KEY_4 if ctrlPressed => wrapS = GL11.GL_REPEAT;          true
+
+            case Keyboard.KEY_A => texCoord += 0.1f; true
+            case Keyboard.KEY_S => texCoord -= 0.1f; true
+
+            case Keyboard.KEY_Z => minFilter = GL11.GL_LINEAR;  true
+            case Keyboard.KEY_X => minFilter = GL11.GL_NEAREST; true
+
+            case Keyboard.KEY_C => magFilter = GL11.GL_LINEAR;  true
+            case Keyboard.KEY_V => magFilter = GL11.GL_NEAREST; true
+
+            case _ => false
+          }
+        }
+      }
+    }
+    stateChanged
   }
 }
