78105c93a2f4642664cead9ebd6c4927e396c89b
Aleksandar Prokopec <axel22@gmail.com>
2014-03-14 09:56:34 +0100

    Adding another missing apply in `macroApplication` match.

--- src/macroglex/scala/org/macrogl/ex/MeshBuffer.scala
+++ src/macroglex/scala/org/macrogl/ex/MeshBuffer.scala
@@ -14,7 +14,7 @@ object MeshBuffer {
   def computing[U: c.WeakTypeTag](c: Context)(f: c.Expr[Unit => U])(glex: c.Expr[Macroglex]): c.Expr[Unit] = {
     import c.universe._
 
-    val Apply(TypeApply(Select(Apply(Apply(_, List(mesh)), List(layoutIndex)), _), _), _) = c.macroApplication
+    val Apply(Apply(TypeApply(Select(Apply(Apply(_, List(mesh)), List(layoutIndex)), _), _), _), _) = c.macroApplication
 
     val r = reify {
       glex.splice.bindShaderStorageBuffer(Macroglex.GL_SHADER_STORAGE_BUFFER, (c.Expr[Int](layoutIndex)).splice, (c.Expr[MeshBuffer](mesh)).splice.token)
