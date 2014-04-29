62aa238619c0affc4a3ce693cab80484287937e1
Joël Rossier <joel.rossier@epfl.ch>
2014-04-29 21:25:59 +0200

    Corrected scalajs-dom version

--- project/Build.scala
+++ project/Build.scala
@@ -112,7 +112,7 @@ object MacroGLBuild extends Build {
     },
     libraryDependencies ++= Seq(
       "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test",
-      "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.4-SNAPSHOT"
+      "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.4"
     )
   )
   
@@ -138,7 +138,7 @@ object MacroGLBuild extends Build {
     scalaSource in Compile := baseDirectory.value / ".." / "src" / "buffer" / "scala",
     libraryDependencies ++= Seq(
       "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test",
-      "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.4-SNAPSHOT"
+      "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.4"
     )
   )
   
